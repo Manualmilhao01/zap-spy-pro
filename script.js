60e88d78ee9e7c3f88d869e1d3cec50ba598b817
Manualmilhao01 <vndersonreall@gmail.com>
2024-09-15 22:55:20 -0300

    new item

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ function redirectAfterDelay(url ) {
     window.location.href = url; 
   } ); 
 }
+document.addEventListener('DOMContentLoaded', function() {
+  const phoneInput = document.getElementById('phone');
+  const submitBtn = document.getElementById('submitBtn');
+
+  // Função para validar o número de telefone
+  function validatePhoneNumber(number) {
+      const phoneRegex = /^\d{10,15}$/; // Ajuste a regex conforme a necessidade
+      return phoneRegex.test(number);
+  }
+
+  // Função para habilitar ou desabilitar o botão
+  function toggleButton() {
+      const phoneNumber = phoneInput.value;
+      if (validatePhoneNumber(phoneNumber)) {
+          submitBtn.classList.remove('hidden');
+      } else {
+          submitBtn.classList.add('hidden');
+      }
+  }
+  phoneInput.addEventListener('input', toggleButton);
+});
